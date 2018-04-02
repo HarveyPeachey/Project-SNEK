8641b48353c1c7782dac5b0c68768fe53505b9e2
Theruler333 <harveypeachey@harveys-MacBook-Pro.local>
2018-04-02 19:25:54 +0100

    Added debugging display for snake head sprite and some variables, Snake head now stays in grid and delays it’s movement if it hasn’t reach a grid boundary. Removed tween for now to look back at a later date

--- SRC/js/game.js
+++ SRC/js/game.js
@@ -1,4 +1,4 @@
-var snake, initialSnakeSize, food, squareSize, score, speed, direction, cursors, newDirection;
+var snake, initialSnakeSize, food, squareSize, score, speed, direction, cursors, newDirection, newGridSquare;
 
 
 var Game = {
@@ -17,23 +17,24 @@ var Game = {
     create: function () {
 
         snake = [];             // This is a stack to store the snake parts
-        initialSnakeSize = 4;   // This is the size of the body of the snake
+        initialSnakeSize = 2;   // This is the size of the body of the snake
         food = {};              // Object for the food piece
         squareSize = 50;        // Size of the grid in pixels should be same as image size of snake sprites
         score = 0;              // Stores the score of the player
         direction = 'up';       // Chooses the initial direction of the snake
-        speed = 3;
+        speed = 2;
 
 
         // Set up a Phaser controller for keyboard input.
         cursors = game.input.keyboard.createCursorKeys();
 
-
+        // Adds background to game
         game.add.sprite(0, 0, 'background');
-        game.add.sprite(300, 150, 'food');
+        // Adds food item to game
+        food = game.add.sprite(0, 0, 'food');
 
 
-
+        // Creates the initial snake based on initialSnakeSize that is set
         for(var i = 0; i < initialSnakeSize; i++) {
             if (i == 0) {
                 snake[i] = game.add.sprite(150, 500, 'shead');
@@ -53,30 +54,56 @@ var Game = {
     },
 
     update: function () {
+
+        //--- Rotates snake head in correct direction also stops illegal moves ---
         if (cursors.right.isDown && direction!='left') {
             newDirection = 'right';
-            // snake[0].angle = 90;
-            game.add.tween(snake[0]).to( { angle: 90 }, 200, Phaser.Easing.Linear.None, true);
+            snake[0].angle = 90;
+            // *PUT IN FUNCTION
+            if (direction == 'up') {
+                newGridSquare =  snake[0].y - (snake[0].y % squareSize);
+            }
+            else if (direction == 'down') {
+                newGridSquare =  snake[0].y + (squareSize - (snake[0].y % squareSize));
+            }
         }
         else if (cursors.left.isDown && direction!='right') {
             newDirection = 'left';
-            // snake[0].angle = -90;
-            game.add.tween(snake[0]).to( { angle: -90 }, 200, Phaser.Easing.Linear.None, true);
+            snake[0].angle = -90;
+            if (direction == 'up') {
+                newGridSquare =  snake[0].y - (snake[0].y % squareSize);
+            }
+            else if (direction == 'down') {
+                newGridSquare =  snake[0].y + (squareSize - (snake[0].y % squareSize));
+            }
         }
         else if (cursors.up.isDown && direction!='down') {
             newDirection = 'up';
-            // snake[0].angle = 0;
-            game.add.tween(snake[0]).to( { angle: 0 }, 200, Phaser.Easing.Linear.None, true);
+            snake[0].angle = 0;
+            if (direction == 'left') {
+                newGridSquare =  snake[0].x - (snake[0].x % squareSize);
+            }
+            else if (direction == 'right') {
+                newGridSquare =  snake[0].x + (squareSize - (snake[0].x % squareSize));
+            }
+
         }
         else if (cursors.down.isDown && direction!='up') {
             newDirection = 'down';
-            // snake[0].angle = 180;
-            game.add.tween(snake[0]).to( { angle: 180 }, 200, Phaser.Easing.Linear.None, true);
+            snake[0].angle = 180;
+            if (direction == 'left') {
+                newGridSquare =  snake[0].x - (snake[0].x % squareSize);
+            }
+            else if (direction == 'right') {
+                newGridSquare =  snake[0].x + (squareSize - (snake[0].x % squareSize));
+            }
         }
 
-        if(newDirection){
+        if (snake[0].y == newGridSquare || snake[0].x == newGridSquare) {
             direction = newDirection;
         }
+
+        //--- Changes direction in which snake segments move ---
 
         if (direction == 'right') {
             for (var i = 0; i < snake.length; i++) {
@@ -98,6 +125,11 @@ var Game = {
                 snake[i].y += speed;
             }
         }
+    },
+
+    render: function () {
+        game.debug.spriteInfo(snake[0], 32, 32);
+        game.debug.text(`Direction of head: ${direction} NewDirection: ${newDirection}  NewDirection:${newGridSquare} `, 20, 20, 'yellow', 'Segoe UI');
     }
 }
 
