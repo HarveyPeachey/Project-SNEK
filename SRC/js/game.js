5d1fb6a9bc97532470c4881d379c3eea769c8867
Theruler333 <harveypeachey@pc-202-128.customer.pin.mainstay.lan>
2018-04-20 14:45:27 +0100

    Fixing and adding some comments trying to make it readable. Removed redundant comments and code

--- SRC/js/game.js
+++ SRC/js/game.js
@@ -6,7 +6,6 @@
     speed,
     fSpeed,
     direction,
-    cursors,
     upButton,
     downButton,
     leftButton,
@@ -41,8 +40,8 @@ var Game = {
 
     create: function () {
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        snakeSize = 15;          // This is the size of the body of the snake
-        snakeCorners = [];
+        snakeSize = 15;         // This is the size of the body of the snake
+        snakeCorners = [];      // These the corners that are used when the snake turns
         food = {};              // Object for the food piece
         squareSize = 30;        // Size of the grid in pixels should be same as image size of snake sprites
         score = 0;              // Stores the score of the player
@@ -54,6 +53,13 @@ var Game = {
         for (var i = 0; i < snakeSize; i++) {
             snake[i] = [];
         }
+        // snake[i] is each segment in the snake snake[i][i] is the properties which are:
+        // [0] Snake Sprite
+        // [1] Direction
+        // [2] NewDirection
+        // [3] NewY coordinate
+        // [4] NewX coordinate
+        // [5][i] This is the direction buffer on all parts
 
         // Set up a Phaser controller for keyboard input.
         //cursors = game.input.keyboard.createCursorKeys();
@@ -61,7 +67,6 @@ var Game = {
         downButton = game.input.keyboard.addKey(Phaser.Keyboard.S);
         leftButton = game.input.keyboard.addKey(Phaser.Keyboard.A);
         rightButton = game.input.keyboard.addKey(Phaser.Keyboard.D);
-        pauseButton = game.input.keyboard.addKey(Phaser.Keyboard.P);
 
         // Adds background to game
         game.add.sprite(0, 0, 'background');
@@ -74,8 +79,10 @@ var Game = {
         banner = game.add.sprite(0, 0, 'banner');
         game.physics.enable(banner, Phaser.Physics.ARCADE);
         banner.body.immovable = true;
+
+        /*  --------------------------------------------SNAKE CREATION-------------------------------------------- */
+
         // Creates a group for the snake sprites so that collision detection can work for each piece of the snake
-        
         cGroup = game.add.group();
         sGroup = game.add.group();
         // Loops through and creates the initial snake based on snakeSize that is set
@@ -110,15 +117,15 @@ var Game = {
             sGroup.add(snake[i][0]);
             if (i != 1)
                 cGroup.add(snake[i][0]);
-           // var pausetxt = game.add.text(400, 36, "Mute Music", { font: '15px Arial', fill: '#FFFF00' });
-          //  pausetxt.anchor.set(0.5);
         }
-        //console.log(snake[0][0]);
         // Make the player collide with the bounds of the world
         game.physics.enable(sGroup, Phaser.Physics.ARCADE);
         game.physics.enable(cGroup, Phaser.Physics.ARCADE);
         game.add.text(150, 36, "Player 1 Score: "+p1score.toString(), { font: '15px Arial', fill: '#FFFF00' });
         game.add.text(320, 36, "Player 2 Score: "+p2score.toString(), { font: '15px Arial', fill: '#FFFF00' });
+
+        /*  --------------------------------------------PAUSE BUTTON-------------------------------------------- */
+
         // Create a label to use as a button
         pause_label = game.add.text(48, 30, 'Pause', { font: '15px Arial', fill: '#FFFF00' });
         pause_label.anchor.set(0.5);
@@ -142,6 +149,9 @@ var Game = {
             game.paused = false; pause_label.x = 48; pause_label.y = 30; play_label.x = -15; play_label.y = -15;
 
         }
+
+        /*  --------------------------------------------MUSIC TOGGLE-------------------------------------------- */
+
         var pausetxt = game.add.text(510, 47, "Mute Music", { font: '15px Arial', fill: '#FFFF00' });
         pausetxt.anchor.set(0.5);
         pausetxt.inputEnabled = true;
@@ -149,66 +159,39 @@ var Game = {
         var playtxt = game.add.text(-11, -11, "Mute Music", { font: '15px Arial', fill: '#FFFF00' });
         playtxt.anchor.set(0.5);
         playtxt.inputEnabled = true;
-  
         playtxt.events.onInputDown.add(pown, this);
+
         function down(item) {
-           
-        music.pause();
-                
-        pausetxt.x = -11;
-        pausetxt.y = -11;
-        playtxt.x = 510;
-        playtxt.y = 47
-          
+            music.pause();
+            pausetxt.x = -11;
+            pausetxt.y = -11;
+            playtxt.x = 510;
+            playtxt.y = 47
        }
 
-
-
         function pown(item) {
-            
-        music.play();
-                
-        pausetxt.x = 510;
-        pausetxt.y = 47;
-        playtxt.x = -11;
-        playtxt.y = -11;
+            music.play();
+            pausetxt.x = 510;
+            pausetxt.y = 47;
+            playtxt.x = -11;
+            playtxt.y = -11;
         }
 
     },
 
     update: function () {
-        // game.input.keyboard.onPressCallback = function(pauseButton){
-        //     game.paused = true;
-        //     speed = 0;
-        //     fSpeed = 0;
-        //     console.log("p");
-        // }
-        // if (game.keyboard.Key.lastKey == "pauseButton){
-        //     console.log("test");
-        //     speed = 0;
-        //     fspeed = 0;
-        //     game.paused = true;
-
-        // }
-        // game.input.onDown.add(unpause, self);
-
-        // function unpause(event){
-        // // Only act if paused
-        //     if(game.paused){
-        //         // Calculate the corners of the menu
-            
-        //     }
-        // }
         game.physics.arcade.collide(food, banner);
-        //updateDelay++;
         levelTime = this.game.time.elapsedSecondsSince(startTime).toFixed(3);
+
         /*  --------------------------------------------SNAKE INPUT-------------------------------------------- */
 
         // Rotates snake head in correct direction also stops illegal moves
         if (game.input.keyboard.justPressed(Phaser.Keyboard.RIGHT) && snake[0][1]!='left' && snake[0][1]!='right') {
-            buffer = snake[0][1];
+            // Sets the new direction to right for head segment
             snake[0][2] = 'right';
+            // Rotates the snake sprite to face the right way depending on key press
             snake[0][0].angle = 90;
+            // Calculates the next coordinates for when the snake can move on the grid dependant on which direction it's moving
             if (snake[0][1] == 'up') {
                 snake[0][3] =  snake[0][0].y - ((snake[0][0].y + snake[0][0].width*0.5) % squareSize);
             }
@@ -217,8 +200,6 @@ var Game = {
             }
         }
         else if (game.input.keyboard.justPressed(Phaser.Keyboard.LEFT) && snake[0][1]!='right' && snake[0][1]!='left') {
-            buffer = snake[0][1];
-            snake[0][2] = 'left';
             snake[0][0].angle = -90;
             if (snake[0][1] == 'up') {
                 snake[0][3] =  snake[0][0].y - ((snake[0][0].y + snake[0][0].width*0.5) % squareSize);
@@ -228,7 +209,6 @@ var Game = {
             }
         }
         else if (game.input.keyboard.justPressed(Phaser.Keyboard.UP) && snake[0][1]!='down' && snake[0][1]!='up') {
-            buffer = snake[0][1];
             snake[0][2] = 'up';
             snake[0][0].angle = 0;
             if (snake[0][1] == 'left') {
@@ -253,10 +233,11 @@ var Game = {
 
         /*  --------------------------------------------SNAKE MOVEMENT-------------------------------------------- */
 
-        // Checks if the snake heads x and y coordinates have hit the new calculated grid square and changes the direction
 
         for (var i = 0; i < snake.length; i++) {
+            // Checks if the snake heads x and y coordinates have hit the new calculated grid square and changes the direction
             if (snake[i][0].y == snake[i][3] || snake[i][0].x == snake[i][4]) {
+                // Used to calculate when to spawn corners when snake moves it's head
                 if (snake[i][0] == snake[0][0]) {
                     if (snake[0][1] == 'up' && snake[0][2] == 'right' || (snake[0][1] == 'left' && snake[0][2] == 'down')) {
                         snakeCorners.push(game.add.sprite(snake[0][0].x, snake[0][0].y, 'scorner'));
@@ -278,38 +259,41 @@ var Game = {
                         snakeCorners[snakeCorners.length-1].angle = -90;
                     }
                 }
+                // This sets the direction to the new direction which was calculated previously for that piece
                 snake[i][1] = snake[i][2];
+                // Goes through each segment of the snake with the direction of the piece before it to get the snake effect
                 if (i < snake.length-1) {
                     snake[i+1][2] = snake[i][2];
                     snake[i+1][3] = snake[i][3];
                     snake[i+1][4] = snake[i][4];
                 }
+                // Reset the newX and newY ready for it's next recalculation
                 snake[i][3] = -1;
                 snake[i][4] = -1;
             }
 
         }
-        // if (updateDelay % 60 == 0) {
-            // Changes direction in which snake segments move
-            for (var i = 0; i < snake.length; i++) {
-                if (snake[i][1] == 'right') {
-                    snake[i][0].angle = 90;
-                    snake[i][0].x += speed;
-                }
-                else if (snake[i][1] == 'left') { 
-                    snake[i][0].angle = -90;
-                    snake[i][0].x += speed-(speed*2);
-                }
-                else if (snake[i][1] == 'up') {
-                    snake[i][0].angle = 0;
-                    snake[i][0].y += speed-(speed*2);
-                }
-                else if (snake[i][1] == 'down') {
-                    snake[i][0].angle = 180;
-                    snake[i][0].y += speed;
-                }
+        // Changes direction in which snake segments move
+        for (var i = 0; i < snake.length; i++) {
+            if (snake[i][1] == 'right') {
+                snake[i][0].angle = 90;
+                snake[i][0].x += speed;
             }
-        // }
+            else if (snake[i][1] == 'left') {
+                snake[i][0].angle = -90;
+                snake[i][0].x += speed-(speed*2);
+            }
+            else if (snake[i][1] == 'up') {
+                snake[i][0].angle = 0;
+                snake[i][0].y += speed-(speed*2);
+            }
+            else if (snake[i][1] == 'down') {
+                snake[i][0].angle = 180;
+                snake[i][0].y += speed;
+            }
+        }
+
+        // Destroys snake corner sprites when the snake turns it's head and deletes it when the tail passes through it
         if(snakeCorners.length > 0) {
             if (snake[snake.length-1][0].x == snakeCorners[0].x && snake[snake.length-1][0].y == snakeCorners[0].y) {
                 snakeCorners[0].destroy();
@@ -344,7 +328,6 @@ var Game = {
         if (Game.overlapAtOffset(food, sGroup, 0, 0) || Game.overlapAtOffset(food, cGroup, 0, 0)){
             p1Win = true;
             p1score += 3;
-
             lossSound = game.add.audio('lSound');
             lossSound.play();
             game.state.start('GameOver');
@@ -353,7 +336,6 @@ var Game = {
         else if (Game.overlapAtOffset(snake[0][0], cGroup, 0, 0)){
             p1Win = false;
             p2score += 3;
-
             lossSound = game.add.audio('lSound');
             lossSound.play();
             game.state.start('GameOver');
@@ -362,43 +344,36 @@ var Game = {
     },
     /*  --------------------------------------------COLLISION BETWEEN SPRITE BODIES-------------------------------------------- */
 
-    //checks whether two sprites overlap up to a certain offset
+    // Checks whether two sprites overlap up to a certain offset
        overlapAtOffsetSprite: function(object1, object2, offsetX, offsetY) {
-        //var graphics = game.add.graphics(0, 0);
         //if either of the parameters passed aren't sprites
         if (typeof(object1.body) === "undefined" || typeof(object2.body) === "undefined") {
             return false;
         }
-        //creates a new sprite shape from which the overlap will be greater dpeending on the offsets set by the user
+        // Creates a new sprite shape from which the overlap will be greater dpeending on the offsets set by the user
         var bounds1 = new Phaser.Rectangle((object1.position.x + object1.body.offset.x - 0.2 * object1.width / object1.scale.x + offsetX),(object1.position.y + object1.body.offset.y - 0 * object1.height / object1.scale.y + offsetY),(object1.body.width*0.4), (object1.body.height*0.4));
-        //graphics.lineStyle(10, 0xFF0000, 0.8);
-        //graphics.beginFill(0xFF700B, 1);
-        //graphics.drawRect((object1.position.x + object1.body.offset.x - 0.2 * object1.width / object1.scale.x + offsetX),(object1.position.y + object1.body.offset.y - 0 * object1.height / object1.scale.y + offsetY),(object1.body.width*0.4), (object1.body.height*0.4));
-        //graphics.drawRect((object2.position.x + object2.body.offset.x - 0.2 * object2.width / object2.scale.x),(object2.position.y + object2.body.offset.y - 0 * object2.height / object2.scale.y),(object2.body.width*0.6), (object2.body.height*0.6));
-        //once again a larger shape is created
         var bounds2 = new Phaser.Rectangle(object2.position.x + object2.body.offset.x -
             0.2 * object2.width / object2.scale.x,
             object2.position.y + object2.body.offset.y -
             0 * object2.height / object1.scale.y,
             object2.body.width*0.6, object2.body.height*0.6);
-        //game.draw
         return Phaser.Rectangle.intersects(bounds1, bounds2);
     },
-    //this function is used when either of the sprites needed to be checked are a group of sprites
+    // This function is used when either of the sprites needed to be checked are a group of sprites
     overlapAtOffset: function(object1, object2, offsetX, offsetY) {
-        //if the first parameter is a group rather than a singular sprite then loop check the overlap against every sprite
+        // If the first parameter is a group rather than a singular sprite then loop check the overlap against every sprite
         if (object1.physicsType == Phaser.GROUP) {
             for (var i = 0; i < object1.children.length; i++) {
                 if (Game.overlapAtOffset(object1.children[i], object2, offsetX, offsetY))
                     return true;
             }
-            //if the second parameter is a group, then loop
+            // If the second parameter is a group, then loop
         } else if (object2.physicsType == Phaser.GROUP) {
             for (var i = 0; i < object2.children.length; i++) {
                 if (Game.overlapAtOffset(object1, object2.children[i], offsetX, offsetY))
                     return true;
             }
-            //otherwise it is singular
+            // Otherwise it is singular
         } else {
             return Game.overlapAtOffsetSprite(object1, object2, offsetX, offsetY);
         }
@@ -422,10 +397,8 @@ var Game = {
 
     },
 
-    reset: function() {
 
-    },
-
+    // Function used to render some UI elements
     render: function() {
         // game.debug.spriteInfo(snake[0][0], 32, 32);
         // game.debug.text(`Direction of head: ${direction} NewDirection: ${newDirection}  NewGridsquare:${newXGridPos} Ticks:${updateDelay} Timer: ${gameTimer}`, 20, 20, 'yellow', 'Segoe UI');
